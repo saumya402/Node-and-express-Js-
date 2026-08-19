eb0c83776cd0b04b28220f99918f3696f9826504
saumya 402 <psaumya8280@gmail.com>
2026-08-19 19:47:22 +0530

    Made employeed db connection

--- src/controllers/EmployeeController.js
+++ src/controllers/EmployeeController.js
@@ -1,6 +1,11 @@
-const getEmployeeById = (req,res)=>{
-    res.json({message : `Person is ${req.params.name} company ${req.params.company}`})
+const EmployeeModel = require("../models/EmployeeModel")
+const getEmployeeById = (req, res) => {
+    res.json({ message: `Person is ${req.params.name} company ${req.params.company}` })
+}
+const getAllEmp = async (req, res) => {
+    const Emp = await EmployeeModel.find()
+    res.json({ message: "Employee Fetched", data: Emp })
 }
 module.exports = {
-    getEmployeeById
+    getEmployeeById, getAllEmp
 }
--- src/routes/EmployeeRoutes.js
+++ src/routes/EmployeeRoutes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router()
 const EmployeeController = require("../controllers/EmployeeController")
-router.get("/employee/:name/:company", EmployeeController.getEmployeeById)
+// router.get("/employee/:name/:company", EmployeeController.getEmployeeById)
+router.get("/employee",EmployeeController.getAllEmp)
 module.exports = router;
